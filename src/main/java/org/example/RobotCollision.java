f242509d324050ac358d1a3d51de08a81843567e
adriano-de-paula <adrianodepaula01@outlook.com>
2023-09-26 17:05:37 -0300

    test: create firsr test for this exercise

--- src/main/java/org/example/RobotCollision.java
+++ src/main/java/org/example/RobotCollision.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.lang.Math;
+
 public class RobotCollision {
+    public static String calculateCollisionPosition(int x1, int y1, int a, int f, int d){
+        if (a < -90 || a > 90 || f < 0 || f > 180 || d < 0){
+            return "Erro";
+        }
+
+        double angleInRadians = Math.toRadians(a);
+
+        int x = (int) (x1 + d * Math.cos(angleInRadians));
+        int y = (int) (y1 + d * Math.sin(angleInRadians));
+
+        return "(" + x + "," + y + ")";
+    }
 }
--- src/test/java/org/example/RobotCollisionTest.java
+++ src/test/java/org/example/RobotCollisionTest.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class RobotCollisionTest {
-    
+public class RobotCollisionTest {
+    @Test
+    @DisplayName("Test calculate collision Position Forward")
+    void testCalculateCollisionPositionForward(){
+        RobotCollision robotCollision = new RobotCollision();
+        assertThat(robotCollision.calculateCollisionPosition(0, 0, 45, 90,5))
+                .isEqualTo("(3,3)");
+    }
 }
