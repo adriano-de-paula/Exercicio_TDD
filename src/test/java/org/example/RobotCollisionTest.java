e06ca44ba9df46d6b0d05a5b170a21914274ce11
adriano-de-paula <adrianodepaula01@outlook.com>
2023-09-26 19:28:32 -0300

    test: create second test in robotCollisionTest

--- src/test/java/org/example/RobotCollisionTest.java
+++ src/test/java/org/example/RobotCollisionTest.java
@@ -1,16 +1,30 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RobotCollisionTest {
+    private  RobotCollision robotCollision;
+
+    @BeforeEach
+    void setUp(){
+       robotCollision = new RobotCollision();
+    }
+
     @Test
     @DisplayName("Test calculate collision Position Forward")
     void testCalculateCollisionPositionForward(){
-        RobotCollision robotCollision = new RobotCollision();
         assertThat(robotCollision.calculateCollisionPosition(0, 0, 45, 90,5))
                 .isEqualTo("(3,3)");
+    }
+
+    @Test
+    @DisplayName("Test calculate collision Position Right")
+    void testCalculateCollisionPositionRight(){
+        assertThat(robotCollision.calculateCollisionPosition(0, 0, 90, 90,5))
+                .isEqualTo("(0,5)");
     }
 }
