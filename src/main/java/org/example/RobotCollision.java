6453939cfc1421725d61fcc913f8313dab0aa329
adriano-de-paula <adrianodepaula01@outlook.com>
2023-09-26 20:00:46 -0300

    refactor:I made the code more organized

--- src/main/java/org/example/RobotCollision.java
+++ src/main/java/org/example/RobotCollision.java
@@ -7,12 +7,24 @@ public class RobotCollision {
         if (a < -90 || a > 90 || f < 0 || f > 180 || d < 0){
             return "Erro";
         }
+        double angleInRadians = getAngleInRadians(a);
 
-        double angleInRadians = Math.toRadians(a);
-
-        int x = (int) (x1 + d * Math.cos(angleInRadians));
-        int y = (int) (y1 + d * Math.sin(angleInRadians));
+        int x = getPositionX(x1, d, angleInRadians);
+        int y = getPositionY(y1, d, angleInRadians);
 
         return "(" + x + "," + y + ")";
     }
+
+    private static int getPositionY(int y1, int d, double angleInRadians) {
+        return (int) (y1 + d * Math.sin(angleInRadians));
+    }
+
+    private static int getPositionX(int x1, int d, double angleInRadians) {
+        return (int) (x1 + d * Math.cos(angleInRadians));
+    }
+
+    private static double getAngleInRadians(int a) {
+        return Math.toRadians(a);
+    }
+
 }
